02905ec6035fd8ad1b0d0099ad657964638343c5
collide2954 <collide2954@proton.me>
2025-01-10 18:18:59 -0600

    Refactor feed processing into modular functions

--- feed/feed.go
+++ feed/feed.go
@@ -39,37 +39,50 @@ func (fh *Handler) CheckFeeds(ctx context.Context, onNewArticle func(title, link
 			return
 		case <-ticker.C:
 			log.Printf("starting feed scan")
-			for _, feedURL := range fh.config.URLs {
-				feed, fetchErr := rss.Fetch(feedURL)
-				if fetchErr != nil {
-					log.Printf("failed fetching feed: %v", fetchErr)
-					continue
-				}
-
-				for _, item := range feed.Items {
-					var seen bool
-					queryErr := fh.db.QueryRow("SELECT EXISTS(SELECT 1 FROM seen_articles WHERE id = ?)", item.ID).Scan(&seen)
-					if queryErr != nil {
-						log.Printf("failed querying seen articles: %v", queryErr)
-						continue
-					}
-
-					if !seen && item.Date.After(programStartTime) {
-						onNewArticleErr := onNewArticle(item.Title, item.Link)
-						if onNewArticleErr != nil {
-							continue
-						}
-
-						_, insertErr := fh.db.Exec(
-							"INSERT INTO seen_articles (id, title, link, date) VALUES (?, ?, ?, ?)",
-							item.ID, item.Title, item.Link, item.Date)
-						if insertErr != nil {
-							log.Printf("failed inserting seen article: %v", insertErr)
-							continue
-						}
-					}
-				}
-			}
+			fh.processFeeds(programStartTime, onNewArticle)
 		}
+	}
+}
+
+func (fh *Handler) processFeeds(programStartTime time.Time, onNewArticle func(title, link string) error) {
+	for _, feedURL := range fh.config.URLs {
+		feed, fetchErr := rss.Fetch(feedURL)
+		if fetchErr != nil {
+			log.Printf("failed fetching feed: %v", fetchErr)
+			continue
+		}
+		fh.processFeedItems(feed.Items, programStartTime, onNewArticle)
+	}
+}
+
+func (fh *Handler) processFeedItems(
+	items []*rss.Item, programStartTime time.Time, onNewArticle func(title, link string) error) {
+	for _, item := range items {
+		if fh.isNewArticle(item, programStartTime) {
+			onNewArticleErr := onNewArticle(item.Title, item.Link)
+			if onNewArticleErr != nil {
+				continue
+			}
+			fh.markArticleAsSeen(item)
+		}
+	}
+}
+
+func (fh *Handler) isNewArticle(item *rss.Item, programStartTime time.Time) bool {
+	var seen bool
+	queryErr := fh.db.QueryRow("SELECT EXISTS(SELECT 1 FROM seen_articles WHERE id = ?)", item.ID).Scan(&seen)
+	if queryErr != nil {
+		log.Printf("failed querying seen articles: %v", queryErr)
+		return false
+	}
+	return !seen && item.Date.After(programStartTime)
+}
+
+func (fh *Handler) markArticleAsSeen(item *rss.Item) {
+	_, insertErr := fh.db.Exec(
+		"INSERT INTO seen_articles (id, title, link, date) VALUES (?, ?, ?, ?)",
+		item.ID, item.Title, item.Link, item.Date)
+	if insertErr != nil {
+		log.Printf("failed inserting seen article: %v", insertErr)
 	}
 }
